aeb08dcd30fbb32e676ef4d92a13c9c97c1456f9
Greivin Calvo Aguilar <Greivin Calvo Aguilar>
2016-07-19 15:02:51 -0600

    inicio de proyecto

---
+++ candy_crush/candy_crush/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace candy_crush.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ candy_crush/candy_crush/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="candy_crush.WebApiApplication" Language="C#" %>
